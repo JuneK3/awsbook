95afcde62f91c204778178a938972f82007fc3a7
Junek3 <rlarudrms28@gmail.com>
2021-12-28 15:54:00 +0900

    Initial commit

---
+++ src/test/java/com/rootlab/awsbook/AwsbookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rootlab.awsbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsbookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
