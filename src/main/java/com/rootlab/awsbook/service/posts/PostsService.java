2bb12376883f141c8f51bd728e37a158065ffe6d
Junek3 <rlarudrms28@gmail.com>
2022-01-01 16:29:42 +0900

    Add/Test Register Posts

---
+++ src/main/java/com/rootlab/awsbook/service/posts/PostsService.java
@@ -0,0 +1,19 @@
+package com.rootlab.awsbook.service.posts;
+
+import com.rootlab.awsbook.domain.posts.PostsRepository;
+import com.rootlab.awsbook.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+	private	final PostsRepository postsRepository;
+
+	@Transactional
+	public Long save(PostsSaveRequestDto requestDto){
+		return postsRepository.save(requestDto.toEntity()).getId();
+	}
+
+}
