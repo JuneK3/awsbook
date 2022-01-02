912784246ddb1c3edce5ed46d5d678d737aa5b85
Junek3 <rlarudrms28@gmail.com>
2022-01-02 16:12:37 +0900

    Add/Test Update Posts

--- src/main/java/com/rootlab/awsbook/service/posts/PostsService.java
+++ src/main/java/com/rootlab/awsbook/service/posts/PostsService.java
@@ -1,6 +1,8 @@
 package com.rootlab.awsbook.service.posts;
 
+import com.rootlab.awsbook.domain.posts.Posts;
 import com.rootlab.awsbook.domain.posts.PostsRepository;
+import com.rootlab.awsbook.web.dto.PostsResponseDto;
 import com.rootlab.awsbook.web.dto.PostsSaveRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -16,4 +18,15 @@ public class PostsService {
 		return postsRepository.save(requestDto.toEntity()).getId();
 	}
 
+	@Transactional
+	public Long update(Long id, PostsSaveRequestDto requestDto) {
+		Posts posts = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+		posts.update(requestDto.getTitle(), requestDto.getContent());
+		return id;
+	}
+
+	public PostsResponseDto findById(Long id){
+		Posts entity = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+		return new PostsResponseDto(entity);
+	}
 }
