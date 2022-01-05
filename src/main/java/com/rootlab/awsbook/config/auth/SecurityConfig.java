6d3e67bf7f67b47855dc499b3aba1d569386bb13
Junek3 <rlarudrms28@gmail.com>
2022-01-06 02:05:58 +0900

    Add spring-session-jdbc && Fix Serializable error

--- src/main/java/com/rootlab/awsbook/config/auth/SecurityConfig.java
+++ src/main/java/com/rootlab/awsbook/config/auth/SecurityConfig.java
@@ -2,9 +2,11 @@ package com.rootlab.awsbook.config.auth;
 
 import com.rootlab.awsbook.domain.user.Role;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.session.security.web.authentication.SpringSessionRememberMeServices;
 
 @RequiredArgsConstructor
 @EnableWebSecurity
@@ -24,9 +26,29 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 				.and()
 					.logout()
 						.logoutSuccessUrl("/")
+						.invalidateHttpSession(true).deleteCookies("SESSION")
 				.and()
 					.oauth2Login()
 						.userInfoEndpoint()
 							.userService(customOAuth2UserService);
+
+		//Spring Security Integration with spring-session-jdbc
+		http.rememberMe((rememberMe) -> rememberMe
+				.rememberMeServices(rememberMeServices())
+		);
+	}
+
+	/*
+	spring-session-jdbc 의존성 추가시 session cookie가 SESSION, JSESSIONID 중복 생성됨
+	spring-session-jdbc 공식문서의 Spring Security Integration with spring-session-jdbc 부분 참고
+	ref: https://docs.spring.io/spring-session/reference/spring-security.html#spring-security-rememberme
+	 */
+	@Bean
+	public SpringSessionRememberMeServices rememberMeServices() {
+		SpringSessionRememberMeServices rememberMeServices =
+				new SpringSessionRememberMeServices();
+		// optionally customize
+		rememberMeServices.setAlwaysRemember(true);
+		return rememberMeServices;
 	}
 }
--- src/main/java/com/rootlab/awsbook/config/auth/dto/SessionUser.java
+++ src/main/java/com/rootlab/awsbook/config/auth/dto/SessionUser.java
@@ -3,8 +3,10 @@ package com.rootlab.awsbook.config.auth.dto;
 import com.rootlab.awsbook.domain.user.User;
 import lombok.Getter;
 
+import java.io.Serializable;
+
 @Getter
-public class SessionUser {
+public class SessionUser implements Serializable {
 	private String name;
 	private String email;
 	private String picture;
