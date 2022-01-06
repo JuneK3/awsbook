b8554e9435644c16688f6a606dccd79997cd1d0f
Junek3 <rlarudrms28@gmail.com>
2022-01-07 02:46:47 +0900

    Driver org.mariadb.jdbc.Driver claims to not accept jdbcUrl 오류

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -42,5 +42,6 @@ JAR_NAME=$(ls -tr $REPOSITORY/ | grep jar | head -n 1)
 echo "> JAR Name: $JAR_NAME"
 
 nohup java -jar \
-    -Dspring.config.location=classpath:/application.properties,/home/ssm-user/app/application-oauth.properties \
+    -Dspring.config.location=classpath:/application.properties,classpath:/application-real.properties,/home/ssm-user/app/application-oauth.properties,/home/ssm-user/app/application-real-db.properties \
+    -Dspring.profiles.active=real \
     $REPOSITORY/$JAR_NAME 2>&1 &
